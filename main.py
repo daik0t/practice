bc6533f5f4b8bf6dd6adf21c4c6fa5cbe05e4526
daikot <daiko4@mail.ru>
2025-06-30 16:31:14 +0700

    Action 1

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
 import cv2 as cv
 
-print("1) Загрузить изображение\n" \
-      "2) Сделать снимок с веб-камеры")
+def viewImage(image, name_of_window):
+    cv.namedWindow(name_of_window, cv.WINDOW_NORMAL)
+    cv.resizeWindow(name_of_window, 800, 500)
+    cv.imshow(name_of_window, image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
-action = input("Выюерите действие: ")
+while True:
+      print("1) Загрузить изображение\n" \
+            "2) Сделать снимок с веб-камеры\n" \
+            "0) Выход")
 
-SHOW_IMG = "1"
-SHOW_RGB = "2"
-SHOW_NEG = "3"
-INC_BRIGHT = "4"
-DRAW_CIRC = "5"
+      action = input("Выберите действие: ")
+      UPLOAD_IMG = "1"
+      WEB_PHOTO = "2"
+      EXIT = "0"
 
-if action == SHOW_IMG: pass
-elif action == SHOW_RGB: pass
-elif action == SHOW_NEG: pass
-elif action == INC_BRIGHT: pass
-elif action == DRAW_CIRC: pass
-else: pass
+      if action == UPLOAD_IMG: img = cv.imread("C:/Users/daiko/Desktop/XYI.png")
+      elif action == WEB_PHOTO: pass
+      elif action == EXIT: break
+      else: continue
+
+      print("1) Показать изображение\n" \
+      "2) Показать красный синий или зеленый канал изображения\n" \
+      "3) Показать негативное изображение\n" \
+      "4) Повысить яркость\n" \
+      "5) Нарисовать круг\n" \
+      "0) Выход")
+
+      action = input("Выберите действие: ")
+      SHOW_IMG = "1"
+      SHOW_RGB = "2"
+      SHOW_NEG = "3"
+      INC_BRIGHT = "4"
+      DRAW_CIRC = "5"
+
+      if action == SHOW_IMG:
+            viewImage(img, "Image")
+      elif action == SHOW_RGB: pass
+      elif action == SHOW_NEG: pass
+      elif action == INC_BRIGHT: pass
+      elif action == DRAW_CIRC: pass
+      else: continue
