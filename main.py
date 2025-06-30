c89d23363e72b3d8bf1b0739d32cd1c9e9e9a2a2
daikot <daiko4@mail.ru>
2025-06-30 14:00:46 +0700

    Шаблон программы

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2 as cv
+
+print("1) Загрузить изображение\n" \
+      "2) Сделать снимок с веб-камеры")
+
+action = input("Выюерите действие: ")
+
+SHOW_IMG = "1"
+SHOW_RGB = "2"
+SHOW_NEG = "3"
+INC_BRIGHT = "4"
+DRAW_CIRC = "5"
+
+if action == SHOW_IMG: pass
+elif action == SHOW_RGB: pass
+elif action == SHOW_NEG: pass
+elif action == INC_BRIGHT: pass
+elif action == DRAW_CIRC: pass
+else: pass
