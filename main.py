5a43ff82532bebfe0ef5182c6808893fe112ac6a
daikot <daiko4@mail.ru>
2025-06-30 23:03:37 +0700

    Action 2, 3, 4

--- main.py
+++ main.py
@@ -1,11 +1,52 @@
 import cv2 as cv
 
-def viewImage(image, name_of_window):
+def viewImage(image, name_of_window="Image"):
     cv.namedWindow(name_of_window, cv.WINDOW_NORMAL)
     cv.resizeWindow(name_of_window, 800, 500)
     cv.imshow(name_of_window, image)
     cv.waitKey(0)
     cv.destroyAllWindows()
+
+def red(image, name_of_window="Image"):
+      print("1) red\n" \
+      "2) green \n" \
+      "3) blue ")
+
+      choice = input("Какой канал показать: ")
+      
+      RED = "1"
+      GREEN = "2"
+      BLUE = "3"
+
+      if choice == BLUE:
+            blue = image.copy()
+            # set green and red channels to 0
+            blue[:, :, 1] = 0
+            blue[:, :, 2] = 0
+            viewImage(blue)
+      elif choice == GREEN:
+            green = image.copy()
+            # set blue and red channels to 0
+            green[:, :, 0] = 0
+            green[:, :, 2] = 0
+            viewImage(green)
+      elif choice == RED:
+            red = image.copy()
+            # set blue and green channels to 0
+            red[:, :, 0] = 0
+            red[:, :, 1] = 0
+            viewImage(red)
+      else: print("smth goes wrong")
+
+def increase_brigthness(image):
+      try:
+            beta = float(input("Введите значение на сколько хотите повысить яркость: "))
+            if beta < 0: raise ValueError
+      except ValueError:
+            print("Число должно быть положительным")
+      image = cv.convertScaleAbs(img, alpha=2, beta=beta)
+      viewImage(image)
+
 
 while True:
       print("1) Загрузить изображение\n" \
@@ -17,7 +58,7 @@ while True:
       WEB_PHOTO = "2"
       EXIT = "0"
 
-      if action == UPLOAD_IMG: img = cv.imread("C:/Users/daiko/Desktop/XYI.png")
+      if action == UPLOAD_IMG: img = cv.imread("C:/Users/daiko/Desktop/98802d213532114e6aac07121355e7d3.jpg")
       elif action == WEB_PHOTO: pass
       elif action == EXIT: break
       else: continue
@@ -37,9 +78,14 @@ while True:
       DRAW_CIRC = "5"
 
       if action == SHOW_IMG:
-            viewImage(img, "Image")
-      elif action == SHOW_RGB: pass
-      elif action == SHOW_NEG: pass
-      elif action == INC_BRIGHT: pass
-      elif action == DRAW_CIRC: pass
+            viewImage(img)
+      elif action == SHOW_RGB: 
+            red(img)
+      elif action == SHOW_NEG: 
+            img = cv.bitwise_not(img)
+            viewImage(img)
+      elif action == INC_BRIGHT: 
+            increase_brigthness(img)
+      elif action == DRAW_CIRC: 
+            draw_circle(img)
       else: continue
